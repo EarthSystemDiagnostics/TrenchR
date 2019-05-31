7d72175fc04914dfec416bc9feb27e64107c52f0
Thomas Münch <tmuench@awi.de>
2019-05-31 15:18:15 +0200

    Simplified calculation code.

--- R/RecordCompression.R
+++ R/RecordCompression.R
@@ -12,28 +12,24 @@
 ##' in East Antarctic firn from analysing temporal changes of isotope profiles,
 ##' The Cryosphere, doi:10.5194/tc-11-2175-2017, 2017.
 ##' @export
-RecordCompression <- function(length.in, adv, rate, res = 1) {
+RecordCompression <- function(z.start = 0, adv, length.in, rate, res = 1) {
 
     # input densification rate is in %/m; convert to 1/cm
     rate <- rate / 10000
-    
-    # positions of first record entry during advection
-    pos1 <- seq(0, adv - res, res)
-    # positions of last record entry during advection
-    pos2 <- seq(length.in, length.in + adv - res, res)
 
-    # size of top/bottom bin of record during densification
-    res1 <- res2 <- vector(length = length(pos1))
-    for (ix in 1 : length(res1)) {
-        res1[ix] <- Compression(pos1[ix], pos1[ix] + res, rate)
-        res2[ix] <- Compression(pos2[ix], pos2[ix] + res, rate)
+    # define positions of depth increments
+    pos <- seq(z.start + res / 2, z.start + length.in, res)
+
+    # calculate compression of defined bins
+    compr <- vector(length = length(pos))
+    for (i in 1 : length(pos)) {
+
+        compr[i] <- res *
+            Compression(z1 = pos[i], z2 = pos[i] + adv, rate = rate)
     }
 
-    # average compression
-    bin.size <- (prod(res1) + prod(res2)) / 2
-
-    # average length of record after densification
-    length.out <- bin.size * length.in
+    # compressed length of record
+    length.out <- sum(compr)
 
     # return length change
     return(length.in - length.out)
