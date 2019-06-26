3a6db9615c16bf3c8438c26b529881818ec20aed
Thomas Münch <tmuench@awi.de>
2019-06-26 14:45:08 +0200

    LoopParamSpace: generalised function call; amended docu.

--- R/LoopParamSpace.R
+++ R/LoopParamSpace.R
@@ -4,17 +4,33 @@
 ##' values to modify a given record accordingly, and return the overall minimum
 ##' root mean square deviation (RMSD) to a reference record.
 ##'
-##' Note that for computational efficiency, the implemented order of
+##' Note that all NA's are stripped from the input record before the computation
+##' process. This is problematic for the diffusion calculation in case of a
+##' large number of internal NA's.
+##'
+##' Note further that for computational efficiency, the implemented order of
 ##' modifications is (1) downward advection, (2) diffusion, and (3) compression
 ##' from densification. This is slightly unphysical since the diffusional
-##' smoothing acts over the uncompressed depth scale. However, it affects the
+##' smoothing so acts over the uncompressed depth scale. However, it affects the
 ##' results only slightly in the domain of high diffusion lengths and high
 ##' compression values, and does not affect the main results discussed in Münch
 ##' et al. (2017).
-##' @param TR tbd
-##' @param advSpace tbd
-##' @param sigmaSpace tbd
-##' @param densfSpace tbd
+##' @param rec.in numeric vector with the input record which is modified
+##' according to the given parameter spaces of advection, diffusion and
+##' densification.
+##' @param reference numeric vector with a reference record against which the
+##' root mean square deviation of the modified record is calculated; must be of
+##' the same length as \code{rec.in}.
+##' @param res the depth resolution of \code{rec.in} and \code{reference}.
+##' @param depth the depth scale of the records; must be in the same units as
+##' \code{res}.
+##' @param advSpace numeric vector of downward advection values to loop over; in
+##' the same units as \code{res}; minimum advection values must not be smaller
+##' than \code{res}.
+##' @param sigmaSpace numeric vector of differential diffusion length values to
+##' loop over; in the same units as \code{res}.
+##' @param densfSpace numeric vector of compression values from densification to
+##' loop over; in the same units as \code{res}.
 ##' @return A list with nine components:
 ##' \describe{
 ##'   \item{adv:}{a copy of \code{advSpace};}
@@ -23,13 +39,13 @@
 ##'   \item{adv.opt:}{the optimal advection value;}
 ##'   \item{sigma.opt:}{the optimal diffusion length;}
 ##'   \item{densf.opt:}{the optimal compression value;}
-##'   \item{adv.opt.arr:}{an array of dimension \code{length(sigma)} x
-##'     \code{length(densf)} which contains the optimal advection values at
+##'   \item{adv.opt.arr:}{an array of dimension \code{length(sigmaSpace)} x
+##'     \code{length(densfSpace)} which contains the optimal advection values at
 ##'     which, for fixed compression and diffusion, the RMSD between the
 ##'     reference and the modified input record is minimal;}
-##'   \item{RMSD:}{an array of dimension \code{length(adv)} x
-##'     \code{length(sigma)} x \code{length(densf)} which contains the RMSD value
-##'     between the reference and the modified input record for every
+##'   \item{RMSD:}{an array of dimension \code{length(advSpace)} x
+##'     \code{length(sigmaSpace)} x \code{length(densfSpace)} which contains the
+##'     RMSD value between the reference and the modified input record for every
 ##'     combination of downward advection, diffusion and compression;}
 ##'   \item{RMSD.opt:}{the array \code{RMSD} projected onto the optimal
 ##'     downward-advection value.}
@@ -41,21 +57,37 @@
 ##' Münch, T., et al., Constraints on post-depositional isotope modifications
 ##' in East Antarctic firn from analysing temporal changes of isotope profiles,
 ##' The Cryosphere, doi:10.5194/tc-11-2175-2017, 2017.
+##' @examples
+##' # Run the analysis for Fig. (5) in Münch et al. (2017)
+##' # (note that this takes some amount of computation time):
+##' \donttest{
+##' TR <- prepareTrenchData()$oxy
+##' ParamSpace <- LoopParamSpace(rec.in = TR$mean13_HiRes,
+##'                              reference = TR$mean15_HiRes,
+##'                              res = TR$HiRes, depth = TR$depth_HiRes,
+##'                              advSpace = seq(40, 60, 0.5),
+##'                              sigmaSpace = seq(0, 8, 0.1),
+##'                              densfSpace = seq(0, 10, 0.1))
+##' }
 ##' @export
-LoopParamSpace <- function(TR = prepareTrenchData()$oxy,
-                           advSpace = seq(40, 60, 0.5)/TR$HiRes,
-                           sigmaSpace = seq(0, 8, 0.1),
-                           densfSpace = seq(0, 10, 0.1)) {
+LoopParamSpace <- function(rec.in, reference, res, depth,
+                           advSpace, sigmaSpace, densfSpace) {
 
-    res <- TR$HiRes
+    if (length(rec.in) != length(reference)) {
+        stop("'rec.in' and 'reference' must be of the same length.")
+    }
 
-    noNA <- which(!is.na(TR$mean13_HiRes))
+    if (min(advSpace) < res) {
+        stop("Advection values smaller than 'res' present.")
+    }
+    
+    advSpace <- advSpace / res
 
-    depth <- TR$depth
-    depth_HiRes <- TR$depth_HiRes[noNA]
+    noNA <- which(!is.na(rec.in))
 
-    IN <- TR$mean13_HiRes[noNA]
-    REF <- TR$mean15_HiRes
+    depth <- depth[noNA]
+    IN <- rec.in[noNA]
+    REF <- reference
 
     RMSD <- array(dim = c(length(advSpace), length(sigmaSpace),
                           length(densfSpace)))
@@ -72,9 +104,9 @@ LoopParamSpace <- function(TR = prepareTrenchData()$oxy,
             for (d in densfSpace){
 
                 # compress the diffused record
-                diff.stretch <- TR$mean13_HiRes
-                diff.stretch[noNA] <- CompressRecord(depth_HiRes,
-                                                     diff, stretch = d)$rec
+                diff.stretch <- rec.in
+                diff.stretch[noNA] <- CompressRecord(depth, diff,
+                                                     stretch = d)$rec
 
                 # advect the diffused and stretched record
                 diff.stretch.adv <- Hmisc::Lag(diff.stretch, k)
