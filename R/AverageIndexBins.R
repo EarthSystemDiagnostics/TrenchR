194384b19837392c5dc0c92e4242d0b24e4d4422
Thomas Münch <tmuench@awi.de>
2019-07-18 16:33:53 +0200

    Proper definition of bins from indices.

--- R/AverageIndexBins.R
+++ R/AverageIndexBins.R
@@ -1,11 +1,17 @@
 ##' Means from index binning.
 ##'
 ##' Calculate bin means defined by vector index positions.
+##'
+##' Averaging bins are defined consecutively from the first index position
+##' to the position one before the second index, i.e., such that the right end
+##' of the first bin does not overlap with the left end of the second bin, and
+##' so on. The final bin is treated separately: if the last index coincides
+##' with the last data point of \code{x}, this point is included in the last bin
+##' average, else the bin is treated as the other bins.
 ##' @param x numeric vector for which to compute bin means according to the
 ##' indices given in \code{ind}.
 ##' @param ind numeric vector of indices specifying the binning windows to
-##' average over; i.e. first binning window is from index number 1 to 2, second
-##' binning window from index number 2 to 3, etc.
+##' average over, see details.
 ##' @param na.rm a logical value indicating whether ‘NA’ values should be
 ##' stripped before the computation proceeds. Defaults to \code{TRUE}.
 ##' @return a numeric vector of length \code{length(ind) - 1} with the mean
@@ -13,13 +19,24 @@
 ##' @author Thomas Münch
 ##' @examples
 ##' x <- rnorm(20)
-##' x.bin <- AverageIndexBins(x, ind = c(1, 7, 13, 17, 20))
+##'
+##' # final bin includes final data point:
+##' AverageIndexBins(x, ind = c(1, 7, 13, 17, 20))
+##' # final index is not final data point:
+##' AverageIndexBins(x, ind = c(1, 7, 13, 17))
 ##' @export
 AverageIndexBins <- function(x, ind, na.rm = TRUE) {
+
+    n <- length(ind)
+
+    # if final index is final data point, include it in the last bin average
+    if (ind[n] == length(x)) ind[n] <- ind[n] + 1
     
     means <- vector()
-    for (i in (1 : (length(ind) - 1))) {
-        range <- ind[i] : ind[i + 1]
+    for (i in (1 : (n - 1))) {
+
+        # bins defined from first to one before second index
+        range <- ind[i] : (ind[i + 1] - 1)
         means[i] <- mean(x[range], na.rm = na.rm)
     }
     
