571f80a39f64c6f903ab15c49f2d1c97739ef55f
Thomas Münch <tmuench@awi.de>
2023-09-08 12:05:32 +0200

    add new data handling functions and test them.

---
+++ R/data-handling.R
@@ -0,0 +1,162 @@
+#' Common vertical trench scale
+#'
+#' Easily retrieve the absolute vertical scale that is common to all profiles of
+#' the supplied trench data set. Most commonly this is a depth scale.
+#'
+#' @param data a trench data set following the default structure used in the
+#'   package.
+#' @param var the name of the vertical scale as a character string; defaults to
+#'   \code{"depth"}.
+#' @return numeric vector of the vertical scale.
+#' @author Thomas Münch
+#' @examples
+#' getZ(t13.trench1)
+#' @export
+#'
+getZ <- function(data, var = "depth") {
+
+  if (!var %in% colnames(data)) {
+    stop("Unknown column name for vertical scale.")
+  }
+
+  unique(data[[var]])
+
+}
+
+#' Trench surface profile
+#'
+#' Easily retrieve the profile positions and the corresponding surface heights
+#' for a supplied trench data set.
+#'
+#' @inheritParams getZ
+#' @return a tibble of two variables: the horizontal profile position in the
+#'   first column and the surface heights at each position in the second column.
+#' @author Thomas Münch
+#' @examples
+#' getSurfaceProfile(t13.trench2)
+#' @export
+#'
+getSurfaceProfile <- function(data) {
+
+  data %>%
+    dplyr::filter(sampleNumber == 1) %>%
+    dplyr::select(position = "profilePosition", height = "surfaceHeight")
+
+}
+
+#' 2D trench record
+#'
+#' Obtain a two-dimensional (vertical x horizontal) representation of a certain
+#' variable for a given trench data set.
+#'
+#' @param var character string with the name of the requested variable; must
+#'   match one of the data column variables in \code{data}, or be one of the
+#'   following:
+#'   \describe{
+#'   \item{\code{"dxs"}:}{this will return d-excess data as calculated from
+#'     \code{"dD - 8 * d18O"} given in the trench data, but only if a column
+#'   \code{"dxs"} is not already present.}
+#'   \item{\code{"nssSulfate"}:}{this will return non-sea-salt sulfate data as
+#'     calculated from \code{"Sulfate - 0.252 * Sodium"} given in the trench
+#'     data, but only if a column \code{"nssSulfate"} is not already present.}
+#'   }
+#' @param simplify logical; for \code{FALSE} (the default) the result is
+#'   returned as a tibble, else it is simplified to a matrix.
+#' @inheritParams getZ
+#' @return a tibble or matrix of the requested trench data, with rows indexing
+#'   the vertical scale (most commonly depth) and columns the horizontal profile
+#'   position.
+#' @importFrom dplyr %>%
+#' @importFrom rlang .data
+#' @author Thomas Münch
+#' @examples
+#'
+#' # Per default, oxygen isotope data is returned:
+#' make2D(t13.trench2)
+#'
+#' # Request another variable:
+#' make2D(t13.trench2, var = "dD")
+#' # Obtain the same result as a simple matrix:
+#' make2D(t13.trench2, var = "dD", simplify = TRUE)
+#'
+#' @export
+#'
+make2D <- function(data, var = "d18O", simplify = FALSE) {
+
+  if (var == "dxs" & (!"dxs" %in% colnames(data))) {
+    if ((!"d18O" %in% colnames(data)) | (!"dD" %in% colnames(data))) {
+      stop("Column variable(s) missing to calculate 'dexcess'.", call. = FALSE)
+    }
+    data <- data %>%
+      dplyr::mutate(dxs = .data$dD - 8 * .data$d18O)
+  }
+  if (var == "nssSulfate" & (!"nssSulfate" %in% colnames(data))) {
+    if ((!"Sulfate" %in% colnames(data)) | (!"Sodium" %in% colnames(data))) {
+      stop("Column variable(s) missing to calculate 'nssSulfate'.",
+           call. = FALSE)
+    }
+    data <- data %>%
+      dplyr::mutate(nssSulfate = .data$Sulfate - 0.252 * .data$Sodium)
+  }
+
+  if (!var %in% colnames(data)) {
+    stop("Unknown column variable selected.", call. = FALSE)
+  }
+
+  result <- data %>%
+    dplyr::select("profileName", "sampleNumber", dplyr::all_of(var)) %>%
+    tidyr::pivot_wider(names_from = "profileName",
+                       values_from = dplyr::all_of(var)) %>%
+    dplyr::select(-"sampleNumber")
+
+  if (simplify) {
+    as.matrix(result)
+  } else {
+    result
+  }
+
+}
+
+#' Trench mean profile
+#'
+#' Calculate the mean profile for a certain trench variable by averaging across
+#' the horizontal dimension of the trench data.
+#'
+#' @inheritParams make2D
+#' @param na.rm a logical evaluating to \code{TRUE} or \code{FALSE} indicating
+#'   whether \code{NA} values should be stripped before the computation of the
+#'   mean profile proceeds.
+#' @param df a logical; for \code{TRUE} (the default) the mean profile is
+#'   returned as a tibble where the first column includes the vertical scale
+#'   of the trench data given in the variable \code{zscale}, else the mean
+#'   profile is returned as a simple numeric vector.
+#' @param zscale a character string giving the name of the vertical scale
+#'   variable used in the trench data; defaults to \code{"depth"}; see also
+#'   \code{\link{getZ}}.
+#' @importFrom dplyr %>%
+#' @return a tibble or numeric vector.
+#' @author Thomas Münch
+#' @examples
+#'
+#' # incomplete surface region is stripped
+#' makeMean(t13.trench2)
+#' # mean values are also calculated for incomplete surface region
+#' makeMean(t13.trench2, na.rm = TRUE)
+#'
+#' # get dD data as a simple vector
+#' makeMean(t13.trench2, var = "dD", df = FALSE)
+#'
+#' @export
+#'
+makeMean <- function(data, var = "d18O", na.rm = FALSE,
+                     df = TRUE, zscale = "depth") {
+
+  if (df) {
+    tibble::tibble(
+      getZ(data, var = zscale), rowMeans(make2D(data, var), na.rm = na.rm)) %>%
+      setNames(c(zscale, var))
+  } else {
+    rowMeans(make2D(data, var), na.rm = na.rm)
+  }
+
+}
---
+++ tests/testthat/test-data-handling.R
@@ -0,0 +1,123 @@
+test_that("extraction of depth profile works", {
+  expect_error(getZ(t13.trench1, var = "foo"),
+               "Unknown column name for vertical scale.")
+  expect_equal(getZ(t13.trench1), seq(from = 1.5, by = 3, length.out = 38))
+})
+
+test_that("production of surface profile works", {
+
+  target <- tibble::tibble(
+    position = seq(0, 50, 5),
+    height = c(12, 10, 14, 14.5, 10, 8, 12, 9.5, 5, 0, 2)
+  )
+
+  expect_equal(getSurfaceProfile(t15.trench2), target)
+
+})
+
+test_that("production of 2D profile works", {
+
+  # test error checking
+
+  foo <- dplyr::select(t15.trench1, -d18O)
+  expect_error(make2D(foo), "Unknown column variable selected.")
+  expect_no_error(make2D(foo, var = "dxs"))
+
+  foo <- dplyr::select(t15.trench1, -d18O, -dxs)
+  expect_error(make2D(foo, var = "dxs"),
+               "Column variable(s) missing to calculate 'dexcess'.",
+               fixed = TRUE)
+
+  foo <- dplyr::select(t15.trench1, -dD, -dxs)
+  expect_error(make2D(foo, var = "dxs"),
+               "Column variable(s) missing to calculate 'dexcess'.",
+               fixed = TRUE)
+
+  foo <- dplyr::select(t15.trench1, -d18O, -dD, -dxs)
+  expect_error(make2D(foo, var = "dxs"),
+               "Column variable(s) missing to calculate 'dexcess'.",
+               fixed = TRUE)
+  expect_error(make2D(foo, var = "nssSulfate"),
+               "Column variable(s) missing to calculate 'nssSulfate'.",
+               fixed = TRUE)
+
+  # test d-excess and nssSulfate calculation
+
+  foo <- tibble::tibble(
+    profileName = rep(c("A", "B"), each = 3),
+    sampleNumber = rep(1 : 3, times = 2),
+    d18O = c(-30.61, -42.36, -38.01, -42.66, -51.98, -41.63),
+    dD = c(-240.5, -338.7, -304.7, -335.1, -408.5, -327.7),
+    dxs = c(4.38, 0.18, -0.62, 6.18, 7.34, 5.34)
+  )
+  target <- tibble::tibble(A = c(4.38, 0.18, -0.62), B = c(6.18, 7.34, 5.34))
+  expect_equal(make2D(dplyr::select(foo, -dxs), var = "dxs"), target)
+
+  foo <- tibble::tibble(
+    profileName = rep(c("A", "B"), each = 2),
+    sampleNumber = rep(1 : 2, times = 2),
+    Sulfate = c(20.6, 34.9, 99.5, 106.1),
+    Sodium = c(234.7, 217.3, 99.6, 178.1),
+    nssSulfate = c(-38.54, -19.86, 74.4, 61.22)
+  )
+  target <- tibble::tibble(A = c(-38.54, -19.86), B = c(74.4, 61.22))
+  expect_equal(
+    round(make2D(dplyr::select(foo, -nssSulfate), var = "nssSulfate"),
+          digits = 2), target)
+
+  # test purely synthetic data
+
+  foo <- tibble::tibble(
+    profileName = rep(c("A", "B", "C"), each = 2),
+    sampleNumber = rep(1 : 2, times = 3),
+    foovar1 = c(4, 7, 1, 25, 2, 56),
+    foovar2 = c(35, 875, 213, 76, 3, 24)
+  )
+
+  target <- tibble::tibble(A = c(4, 7), B = c(1, 25), C = c(2, 56))
+  expect_equal(make2D(foo, var = "foovar1"), target)
+  target <- as.matrix(
+    tibble::tibble(A = c(35, 875), B = c(213, 76), C = c(3, 24)))
+  expect_equal(make2D(foo, var = "foovar2", simplify = TRUE), target)
+
+  # tests with actual trench data
+
+  target <- matrix(c(-43.56, NA, -43.16, -47.11,
+                     -43.71, -37.83, -45.9, -47.98,
+                     -43.33, -42.28, -47.73, -47.03,
+                     -47.04, -47.02, -47.96, -46.34,
+                     -49.73, -49.33, -47.31, -42.23),
+                   nrow = 5, ncol = 4, byrow = TRUE)
+  colnames(target) <- c("T13-2-01", "T13-2-02", "T13-2-03", "T13-2-04")
+
+  expect_equal(make2D(t13.trench2, simplify = TRUE)[3 : 7, ], target)
+
+  target <- matrix(c(-408.2, -381,
+                     -385.3, -356.4,
+                     -343.1, -343.8,
+                     -328.8, -369.8),
+                   nrow = 4, ncol = 2, byrow = TRUE)
+  colnames(target) <- c("T15-1-L", "T15-1-M")
+
+  expect_equal(
+    make2D(t15.trench1, var = "dD", simplify = TRUE)[10 : 13, 11 : 12],
+    target)
+
+})
+
+test_that("production of mean profile works", {
+
+  foo <- dplyr::filter(t13.trench2, sampleNumber <= 3, profilePosition < 30)
+  foo <- dplyr::mutate(foo, foovar = c(1 : 3, c(NA, 3, 4), 3 : 5))
+
+  # return type should be a simple vector
+  target <- c(NA, 3, 4)
+  expect_equal(makeMean(foo, var = "foovar", df = FALSE), target)
+  target <- c(2, 3, 4)
+  expect_equal(makeMean(foo, var = "foovar", na.rm = TRUE, df = FALSE), target)
+
+  # return type should be a tibble
+  target <- tibble::tibble(depth = c(1.5, 4.5, 7.5), foovar = target)
+  expect_equal(makeMean(foo, var = "foovar", na.rm = TRUE), target)
+
+})
