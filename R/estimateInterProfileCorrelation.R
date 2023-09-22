4de57b04cb3c9c4919b842dc924697b1fe65a49b
Thomas Münch <tmuench@awi.de>
2023-09-22 16:01:17 +0200

    add function to get prf correlation vs distance.

---
+++ R/estimateInterProfileCorrelation.R
@@ -0,0 +1,96 @@
+#' Correlation between profiles depending on distance
+#'
+#' Estimate the pairwise correlation between trench profiles as a function of
+#' the distance between the profiles.
+#'
+#' For a given distance, the function searches the trench data set for all pairs
+#' which are separated horizontally by this distance, allowing for a given
+#' tolerance in separation distance. It is ensured that profile pairs are
+#' counted only once. For every found pair, the correlation between the two
+#' profiles is calculated using pairwise complete observations, and the
+#' correlations are averaged across all pairs. Uncertainty of the mean
+#' correlation is given by the standard deviation across the correlations from
+#' all pairs, and by the standard error. The latter is calculated using the
+#' number of found pairs, or the effective horizontal number of profiles
+#' (degrees of freedom; as determined from the given horizontal autocorrelation
+#' at lag 1), whichever number is smaller.
+#'
+#' @param trench a trench data set in form of a matrix or a data frame, where
+#'   columns represent the horizontal trench profile positions and rows
+#'   represent the vertical scale of the data; see also \code{\link{make2D}}.
+#' @param profilePosition vector of the horizontal trench profile positions; see
+#'   also \code{\link{getSurfaceProfile}}.
+#' @param distances vector of profile spacings for which profile pairs shall be
+#'   seeked and analysed for their correlation; must have the same units as
+#'   \code{profilePosition}.
+#' @param rangeTol relative tolerance allowed for profile spacings; i.e. for a
+#'   given spacing d, all profile pairs separated by d * (1 +/- rangeTol) are
+#'   analysed.
+#' @param a1 horizontal autocorrelation of the trench data at lag 1 to obtain an
+#'   effective number of profiles used for the error estimates (see details).
+#' @return a tibble of five variables: the analysed profile spacings, the found
+#'   number of pairs, the mean correlation between the pairs, and the standard
+#'   deviation and standard error of the mean correlation (see details).
+#' @author Thomas Münch
+#' @examples
+#'
+#' IPC <- make2D(t13.trench2) %>%
+#'   estimateInterProfileCorrelation(
+#'     profilePosition = getSurfaceProfile(t13.trench2)$position,
+#'     distances = c(10, 30))
+#'
+#' @export
+#'
+estimateInterProfileCorrelation <- function(trench, profilePosition, distances,
+                                            rangeTol = 0.05, a1 = 0) {
+
+  if (!is.matrix(trench) & !is.data.frame(trench)) {
+    stop("Input 'trench' must be a matrix or a data frame.")
+  }
+
+  results <- list()
+  meanCor <- stdDev <- stdErr <- Nfound <- vector()
+
+  count <- 1
+
+  for (iRange in distances) {
+
+    for (iPrf in (1 : ncol(trench))) {
+
+      # search for target profiles at distance iRange +- tolerance
+      iTargets <- which(
+        (profilePosition <= profilePosition[iPrf] + (1 + rangeTol) * iRange) &
+        (profilePosition >= profilePosition[iPrf] + (1 - rangeTol) * iRange))
+
+      # remove 1st profile if included in list of target profiles
+      startPrf <- which(iTargets == iPrf)
+      if (length(startPrf) != 0) iTargets <- iTargets[-startPrf]
+      
+      if (length(iTargets) == 0) {
+
+        # no pairs found
+        results[[iPrf]] <- NA
+
+      } else {
+        
+        results[[iPrf]] <- cor(trench[, c(iPrf, iTargets)],
+                               use = "pairwise.complete.obs")[1, -1]
+      }
+    }
+
+    tmp <- unlist(results)[!is.na(unlist(results))]
+
+    Nfound[count]  <- length(tmp)
+    meanCor[count] <- mean(tmp)
+    stdDev[count]  <- sd(tmp)
+    stdErr[count]  <- sd(tmp) /
+      sqrt(min(Nfound[count], stattools::getEffectiveDOF(ncol(trench), a1)))
+
+    count <- count + 1
+
+  }
+
+  tibble::tibble(distances = distances, N = Nfound,
+                 cor = meanCor, sd = stdDev, se = stdErr)
+
+}
---
+++ tests/testthat/test-estimateInterProfileCorrelation.R
@@ -0,0 +1,57 @@
+context("inter-profile-correlation")
+
+test_that("inter profile correlation calculation works", {
+
+  msg <- "Input 'trench' must be a matrix or a data frame."
+  expect_error(
+    estimateInterProfileCorrelation(
+      numeric(1), profilePosition = 1, distances = 1),
+    msg, fixed = TRUE)
+  expect_error(
+    estimateInterProfileCorrelation(
+      character(1), profilePosition = 1, distances = 1),
+    msg, fixed = TRUE)
+  expect_error(
+    estimateInterProfileCorrelation(
+      list(foo = 1, bar = 2), profilePosition = 1, distances = 1),
+    msg, fixed = TRUE)
+
+    profilePosition <- c(0, 1, 2, 4)
+    trench <- data.frame(
+      a = 1 : 4, b = 1 : 4, c = 1 : 4, d = 4 : 1)
+
+    actual1 <- estimateInterProfileCorrelation(
+      trench, profilePosition, distances = 1 : 3)
+    actual2 <- estimateInterProfileCorrelation(
+      as.matrix(trench), profilePosition, distances = 1 : 3)
+    expected1 <- tibble::tibble(
+      distances = 1 : 3, N = c(2, 2, 1), cor = c(1, 0, -1),
+      sd = c(0, sqrt(2), NA), se = c(0, 1, NA))
+    actual3 <- estimateInterProfileCorrelation(
+      as.matrix(trench), profilePosition, distances = 1 : 3, a1 = 0.9) %>%
+      round(digits = 5)
+    expected2 <- tibble::tibble(
+      distances = 1 : 3, N = c(2, 2, 1), cor = c(1, 0, -1),
+      sd = c(0, sqrt(2), NA), se = c(0, sqrt(2) / sqrt(1.134913), NA)) %>%
+      round(digits = 5)
+
+    expect_equal(actual1, expected1)
+    expect_equal(actual2, expected1)
+    expect_equal(actual3, expected2)
+
+    profilePosition <- c(0, 0.95, 2.1, 3.71)
+
+    actual1 <- estimateInterProfileCorrelation(
+      trench, profilePosition, distances = 1 : 3, rangeTol = 0)
+    actual2 <- estimateInterProfileCorrelation(
+      as.matrix(trench), profilePosition, distances = 1 : 3, rangeTol = 0.2)
+
+    expected3 <- tibble::tibble(
+      distances = as.integer(1 : 3), N = as.integer(c(0, 0, 0)),
+      cor = c(NaN, NaN, NaN),
+      sd = as.numeric(rep(NA, 3)), se = as.numeric(rep(NA, 3)))
+
+    expect_equal(actual1, expected3)
+    expect_equal(actual2, expected1)
+    
+})
