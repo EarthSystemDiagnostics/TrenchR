2c5217d5bf627f8efd7a942dc6ffbc890cda70ac
Thomas Münch <tmuench@awi.de>
2023-10-05 10:25:04 +0200

    add function to estimate inter-trench correlation.

---
+++ R/estimateInterTrenchCorrelation.R
@@ -0,0 +1,75 @@
+#' Pairwise correlation between trenches
+#'
+#' Calculate the pairwise correlations of all records in a trench with all
+#' records in a second trench; optionally allowing for relative shifts of the
+#' records with respect to each other in order to maximize their correlations.
+#'
+#' @param t1 a generic trench data set.
+#' @param t2 a second generic trench data set.
+#' @param var character string with the name of the trench variable for which to
+#'   compute the correlations; see also \code{\link{make2D}}.
+#' @param optimize logical; set to \code{TRUE} to allow for relative shifts
+#'   of the records, specified by \code{lag}, to maximize their correlations.
+#' @param lag a vector of lags, i.e. positive and negative index shifts, used
+#'   when \code{optimize = TRUE} to shift the records of one trench relative to
+#'   the records of the other trench in order to find their maximum
+#'   correlations.
+#' @importFrom dplyr %>%
+#' @return For \code{optimize = FALSE}, a matrix of all pairwise correlations
+#'   between the records in \code{t1} and in \code{t2}, where rows tabulate the
+#'   profile positions of \code{t1} and columns those of \code{t2}. For
+#'   \code{optimize = TRUE}, a list is returned of two matrices in the same
+#'   format, where component \code{cor} contains the optimized correlations
+#'   and component \code{i} contains the index shifts corresponding to the
+#'   maximum correlations.
+#' @author Thomas Münch
+#' @examples
+#'
+#' raw <- estimateInterTrenchCorrelation(t13.trench1, t13.trench2)
+#' opt <- estimateInterTrenchCorrelation(t13.trench1, t13.trench2,
+#'                                       optimize = TRUE, lag = -4 : 4)
+#'
+#' mean(raw)
+#' mean(opt$cor)
+#'
+#' @export
+#'
+estimateInterTrenchCorrelation <- function(t1, t2, var = "d18O",
+                                           optimize = FALSE, lag = NULL) {
+
+  if (!optimize) {
+    
+    x <- make2D(t1, var = var)
+    y <- make2D(t2, var = var)
+    cor(x, y, use = "pairwise.complete.obs")
+
+  } else {
+
+    if (!length(lag)) {
+      stop("Need vector of lags to optimize correlations.", call. = FALSE)
+    }
+
+    x <- make2D(t1, var = var, simplify = TRUE)
+    y <- make2D(t2, var = var, simplify = TRUE)
+    n <- ncol(x)
+    m <- ncol(y)
+
+    nms <- list(unique(t1$profileName), unique(t2$profileName))
+
+    optc <- opti <- matrix(nrow = n, ncol = m)
+    dimnames(optc) <- dimnames(opti) <- nms
+
+    for (i in 1 : n) {
+      for (j in 1 : m) {
+
+        opt <- stattools::ShiftCorrelation(x[, i], y[, j], shifts = lag)
+        optc[i, j] <- opt$cor
+        opti[i, j] <- opt$i
+
+      }
+    }
+
+    list(cor = optc, i = opti)
+  }
+
+}
---
+++ tests/testthat/test-estimateInterTrenchCorrelation.R
@@ -0,0 +1,45 @@
+context("inter-trench-correlation")
+
+test_that("inter-trench correlation works", {
+
+  set.seed(20231005)
+
+  a <- rnorm(100)
+  b <- rnorm(100)
+  c <- rnorm(100)
+  d <- rnorm(100)
+  e <- rnorm(100)
+  f <- rnorm(100)
+
+  t1 <- tibble::tibble(
+    profileName = rep(letters[1 : 2], each = 100),
+    sampleNumber = rep(1 : 100, times = 2),
+    depth = rep(1 : 100, times = 2),
+    foo = c(a, b))
+  t2 <- tibble::tibble(
+    profileName = rep(letters[3 : 6], each = 100),
+    sampleNumber = rep(1 : 100, times = 4),
+    depth = rep(1 : 100, times = 4),
+    foo = c(c, d, e, f))
+
+  expected1 <- matrix(
+    data = c(cor(a, c), cor(a, d), cor(a, e), cor(a, f),
+             cor(b, c), cor(b, d), cor(b, e), cor(b, f)),
+    nrow = 2, ncol = 4, byrow = TRUE,
+    dimnames = list(letters[1 : 2], letters[3 : 6]))
+
+  actual1 <- estimateInterTrenchCorrelation(t1, t2, var = "foo")
+  expect_equal(expected1, actual1)
+
+  expected2 <- list(cor = expected1,
+                    i = matrix(0, nrow = 2, ncol = 4, byrow = TRUE,
+                               dimnames = list(letters[1 : 2], letters[3 : 6])))
+  actual2 <- estimateInterTrenchCorrelation(t1, t2, var = "foo",
+                                            optimize = TRUE, lag = 0)
+
+  expect_error(
+    estimateInterTrenchCorrelation(t1, t2, var = "foo", optimize = TRUE),
+    "Need vector of lags to optimize correlations.")
+  expect_equal(expected2, actual2)
+
+})
